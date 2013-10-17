37cdadded98fe67f3a4f4c404b57d1926aac1480
nickruiz <nick@fatcatonline.net>
2013-10-17 16:31:18 -0700

    Cart mobile styling

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -135,7 +135,7 @@ $woocommerce->show_messages();
 
 			<div class="options">
 
-				<input type="submit" class="button" name="update_cart" value="<?php _e( 'Update Cart', 'woocommerce' ); ?>" /> <input type="submit" class="checkout-button button alt" name="proceed" value="<?php _e( 'Proceed to Checkout &rarr;', 'woocommerce' ); ?>" />
+				<input type="submit" class="button" name="update_cart" value="<?php _e( 'Update Cart', 'woocommerce' ); ?>" /> <input type="submit" class="checkout-button button alt" name="proceed" value="<?php _e( 'Checkout &rarr;', 'woocommerce' ); ?>" />
 
 				<?php do_action('woocommerce_proceed_to_checkout'); ?>
 
